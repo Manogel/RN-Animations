adf9dd0bd6c9a855771b5f54409c90495cdbe6d0
Manogel <manoel_ggomes@hotmail.com>
2020-01-23 12:11:11 -0300

    effect  bouciness

--- src/User/index.js
+++ src/User/index.js
@@ -1,31 +1,64 @@
-import React from 'react';
-import { TouchableWithoutFeedback } from 'react-native';
-import Icon from "react-native-vector-icons/FontAwesome";
-import { Container,Thumbnail,
+import React, {useState, useEffect} from 'react';
+import {TouchableWithoutFeedback, Animated} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {
+  Container,
+  Thumbnail,
   Content,
   BioContainer,
   Name,
   Description,
   LikeContainer,
-  LikeText, } from './styles';
+  LikeText,
+} from './styles';
 
-export default function User({ user, onPress}) {
+export default function User({user, onPress}) {
+  const offset = new Animated.ValueXY({x: 0, y: 50});
+  const opacity = new Animated.Value(0);
+  //const [offset, setOffset] = useState(new Animated.ValueXY({x: 0, y: 50}));
+
+  useEffect(() => {
+    //animações em paralelo
+    Animated.parallel([
+      // sprint: efeito bouciness
+      Animated.spring(offset.y, {
+        toValue: 0,
+        speed: 5,
+        bounciness: 20,
+      }),
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 500,
+      })
+    ]).start()
+    
+  }, []);
+
   return (
-    <TouchableWithoutFeedback onPress={onPress}>
-      <Container>
-        <Thumbnail source={{uri: user.thumbnail}} />
-        <Content>
-          <BioContainer>
-            <Name>{user.name.toUpperCase()}</Name>
-  <Description>{user.description}</Description>
-
-          </BioContainer>
-          <LikeContainer>
-            <Icon name="heart" size={12} color="#fff" />
-  <LikeText>{user.likes}</LikeText>
-          </LikeContainer>
-        </Content>
-      </Container>
-    </TouchableWithoutFeedback>
+    <Animated.View
+      style={[
+        {
+          transform: [...offset.getTranslateTransform()],
+        },
+        {
+          opacity
+        }
+      ]}>
+      <TouchableWithoutFeedback onPress={onPress}>
+        <Container>
+          <Thumbnail source={{uri: user.thumbnail}} />
+          <Content>
+            <BioContainer>
+              <Name>{user.name.toUpperCase()}</Name>
+              <Description>{user.description}</Description>
+            </BioContainer>
+            <LikeContainer>
+              <Icon name="heart" size={12} color="#fff" />
+              <LikeText>{user.likes}</LikeText>
+            </LikeContainer>
+          </Content>
+        </Container>
+      </TouchableWithoutFeedback>
+    </Animated.View>
   );
 }
